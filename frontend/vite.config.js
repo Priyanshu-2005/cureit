8bbb4c4a228350a4bdd231e65f7e42ed04bc1900
Priyanshu Agarwal <143310797+Priyanshu-2005@users.noreply.github.com>
2026-03-31 12:43:41 +0530

    Change base path in Vite config to root

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import eslint from "vite-plugin-eslint";
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/cureit",
+  base: "/",
   plugins: [react(), eslint()],
 });
